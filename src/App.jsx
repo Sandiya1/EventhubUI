86da082c404b7f937cf3124a8ebb31c8275dfce6
Sandiya <sandiya1804@gmail.com>
2025-11-27 15:12:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import login from "./pages/login";
-import signup from "./pages/signup";
-import Events from "./pages/Events"; // later
-import OrganizerDashboard from "./pages/OrganizerDashboard";
-import CreateEvent from "./pages/CreateEvent";
-import EditEvent from "./pages/EditEvent";
-import MyRegistrations from "./pages/MyRegistrations";
-import LandingPage from "./pages/LandingPage";
-
-
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Events from "./pages/Events.jsx";
+import OrganizerDashboard from "./pages/OrganizerDashboard.jsx";
+import CreateEvent from "./pages/CreateEvent.jsx";
+import EditEvent from "./pages/EditEvent.jsx";
+import MyRegistrations from "./pages/MyRegistrations.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<login />} />
-        <Route path="/signup" element={<signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/events" element={<Events />} />
         <Route path="/organizer" element={<OrganizerDashboard />} />
         <Route path="/create-event" element={<CreateEvent />} />
         <Route path="/edit-event/:id" element={<EditEvent />} />
         <Route path="/my-registrations" element={<MyRegistrations />} />
         <Route path="/" element={<LandingPage />} />
-        
-
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App; 
+export default App;
